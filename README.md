d2e804e93eebcebe697d8924ab52c20948900d9c
Brian Shamblen <brian@sadabri.com>
2015-03-03 19:01:50 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+meteor-morris
+===============
+A wrapper for version 0.5.1 of the JavaScript charting library [morris.js](http://morrisjs.github.io/morris.js/)
+
+##Install
+``` sh
+meteor add morrisjs
+```
---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'bshamblen:morrisjs',
+  version: '0.5.1',
+  // Brief, one-line summary of the package.
+  summary: 'Wrapper for Morris.js version 0.5.1',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/bshamblen/meteor-morrisjs.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.2');
+  api.use(['jquery', 'raphael']);
+  api.addFiles('morris.js', 'client');
+  api.addFiles('morris.css', 'client');
+});
